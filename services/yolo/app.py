37c0f704283473926b21f7914a46210e600195f9
rinaahmd <rinafdlahmd26@gmail.com>
2026-06-10 16:46:34 +0300

    Add CI workflow and prediction time

--- services/yolo/app.py
+++ services/yolo/app.py
@@ -5,8 +5,12 @@ from ultralytics import YOLO
 from PIL import Image
 import sqlite3
 import logging
+import sqlite3
+import logging
 import os
+
 import uuid
+import time
 import shutil
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
@@ -97,6 +101,7 @@ def predict(file: UploadFile = File(...)):
     """
     Predict objects in an image
     """
+    start_time = time.time()
     ext = os.path.splitext(file.filename)[1]
     uid = str(uuid.uuid4())
     original_path = os.path.join(UPLOAD_DIR, uid + ext)
@@ -121,11 +126,13 @@ def predict(file: UploadFile = File(...)):
         bbox = box.xyxy[0].tolist()
         save_detection_object(uid, label, score, bbox)
         detected_labels.append(label)
+    processing_time = round(time.time() - start_time, 2)
 
     return {
         "prediction_uid": uid, 
         "detection_count": len(results[0].boxes),
-        "labels": detected_labels
+        "labels": detected_labels,
+        "time_took": processing_time
     }
 
 @app.get("/prediction/{uid}")
